a0152a3d8ab4b110bef391d9e45aac5ddcfb31db
dwaseem2 <danish.waseem@kpmg.co.uk>
2024-08-01 16:48:29 +0100

    new stuff

---
+++ eval.py
@@ -0,0 +1,47 @@
+import mlflow
+import openai
+import os
+import pandas as pd
+import dagshub
+
+dagshub.init(repo_owner='krishnaik06', repo_name='MLfLow', mlflow=True)
+mlflow.set_tracking_uri("https://dagshub.com/krishnaik06/MLfLow.mlflow")
+eval_data = pd.DataFrame(
+    {
+        "inputs": [
+            "hi how are you?",
+        ],
+        "ground_truth": [
+            "im alright how you doing",
+        ],
+    }
+)
+mlflow.set_experiment("LLM Evaluation")
+with mlflow.start_run() as run:
+    system_prompt = "Answer the following question in two sentences"
+    # Wrap "gpt-4" as an MLflow model.
+    logged_model_info = mlflow.openai.log_model(
+        model="gpt-4",
+        task=openai.chat.completions,
+        artifact_path="model",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": "{question}"},
+        ],
+    )
+
+    # Use predefined question-answering metrics to evaluate our model.
+    results = mlflow.evaluate(
+        logged_model_info.model_uri,
+        eval_data,
+        targets="ground_truth",
+        model_type="question-answering",
+        extra_metrics=[mlflow.metrics.toxicity(), mlflow.metrics.latency(),mlflow.metrics.genai.answer_similarity()]
+    )
+    print(f"See aggregated evaluation results below: \n{results.metrics}")
+
+    # Evaluation result for each data record is available in `results.tables`.
+    eval_table = results.tables["eval_results_table"]
+    df=pd.DataFrame(eval_table)
+    df.to_csv('eval.csv')
+    print(f"See evaluation table below: \n{eval_table}")
