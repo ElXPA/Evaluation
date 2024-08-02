c4c8f9b515cc4bb7bc0691c59ca3e94013841b17
dwaseem2 <danish.waseem@kpmg.co.uk>
2024-08-02 09:44:09 +0100

    tracking for dagshub

--- eval.py
+++ eval.py
@@ -4,8 +4,8 @@ import os
 import pandas as pd
 import dagshub
 
-dagshub.init(repo_owner='krishnaik06', repo_name='MLfLow', mlflow=True)
-mlflow.set_tracking_uri("https://dagshub.com/krishnaik06/MLfLow.mlflow")
+dagshub.init(repo_owner='ElXPA', repo_name='Evaluation', mlflow=True)
+mlflow.set_tracking_uri("https://dagshub.com/ElXPA/Evaluation.mlflow")
 eval_data = pd.DataFrame(
     {
         "inputs": [
